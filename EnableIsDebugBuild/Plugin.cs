1e29da720729dca1673cf62e38e73643efbdaf72
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-08-10 21:17:35 +0800

    chore: init

---
+++ EnableIsDebugBuild/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace EnableIsDebugBuild
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        internal static new ManualLogSource Log;
+
+        public override void Load()
+        {
+            Log = base.Log;
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            // 初始化 Harmony
+            Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // 立刻测试 Debug.isDebugBuild
+            Log.LogInfo($"[Test] Debug.isDebugBuild = {Debug.isDebugBuild}");
+        }
+    }
+
+    // Patch Debug.isDebugBuild getter
+    [HarmonyPatch(typeof(Debug), nameof(Debug.isDebugBuild), MethodType.Getter)]
+    public static class DebugIsDebugBuildPatch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true; // 强制返回 true
+            return false;    // 跳过原始 getter
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EnableIsDebugBuild-BepInEx-IL2CPP
+
+Plugin that set `Debug.isDebugBuild` to `True` for BepInEx 6 IL2CPP.
